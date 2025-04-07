31627691359b5b14130bd52ac7491d8e18dc0a6b
SilasReinagel <silas.reinagel@gmail.com>
2025-04-07 00:05:27 -0500

    Boss Mood Never Goes Below 0

--- src/EnigmaDragonsProject/Assets/Scripts/State/GameState.cs
+++ src/EnigmaDragonsProject/Assets/Scripts/State/GameState.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 [Serializable]
 public sealed class GameState
@@ -144,7 +145,8 @@ public class BossState
 
     public int UpdateMoodAndGetSnapsChanges(int moodChange)
     {
-        CurrentMood += moodChange;
+        // Apply the mood change but ensure it doesn't go below 0
+        CurrentMood = Mathf.Max(0, CurrentMood + moodChange);
         int newMoodTier = GetMoodTier(CurrentMood);
         
         // Check if we've crossed a mood threshold to a higher tier
